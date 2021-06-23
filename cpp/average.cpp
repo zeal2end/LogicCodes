30dc7ace09ae81052443a294cec94069d82eb2ba
zeal2end <vyvivekkumar@gmail.com>
2021-06-23 16:31:56 +0530

    semicolon

--- cpp/average.cpp
+++ cpp/average.cpp
@@ -5,10 +5,11 @@ using vi=vector<int>;
 #define ar array
 int n,average;
 int main(){
+	//good
 	cin >> n;
 	for(int i = 0;i<n;i++){
 		int a;
-		cin >> a
+		cin >> a;
 		average += a;
 	}
 	cout << average/n;
